836601d56f5e4bbb09a25726bc60aed555a2a8d3
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-12-06 20:27:26 +0700

    add access origin

--- server/application/controllers/api/login.php
+++ server/application/controllers/api/login.php
@@ -2,6 +2,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
+require_once FCPATH . 'vendor/autoload.php';
 
 use Restserver\Libraries\REST_Controller;
 
@@ -10,9 +11,24 @@ class Login extends REST_Controller
     function __construct()
     {
         parent::__construct();
+        header('Access-Control-Allow-Origin:*');
+        header("Access-Control-Allow-Headers:X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Request-Method,Authorization");
+        header("Access-Control-Allow-Methods:GET,POST,OPTIONS,PUT,DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
         $this->load->database();
         $this->load->model('M_User');
         $this->load->library('form_validation');
+        $this->load->library('jwt');
+    }
+
+    public function options_get() {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        exit();
     }
 
     function validate()
@@ -46,10 +62,13 @@ class Login extends REST_Controller
             return $this->response($response, 401);
         }
 
+        $token = $this->jwt->encode($email, $password);
+
         $response = array(
             'status_code' => 200,
             'message' => 'Login successful',
             'user_data' => $user,
+            'token' => $token
         );
 
         return $this->response($response);
--- server/application/controllers/api/register.php
+++ server/application/controllers/api/register.php
@@ -10,6 +10,13 @@ class Register extends REST_Controller
     function __construct()
     {
         parent::__construct();
+        header('Access-Control-Allow-Origin:*');
+        header("Access-Control-Allow-Headers:X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Request-Method,Authorization");
+        header("Access-Control-Allow-Methods:GET,POST,OPTIONS,PUT,DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
         $this->load->database();
         $this->load->model('M_User');
         $this->load->library('form_validation');
@@ -19,6 +26,13 @@ class Register extends REST_Controller
         $this->form_validation->set_rules('username', 'Username', 'required|trim|is_unique[user.username]');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
+    }
+
+    public function options_get() {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        exit();
     }
     
     function index_post()
