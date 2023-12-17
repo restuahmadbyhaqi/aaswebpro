663b67e31975ee5b3d4fa3f9fc9d61d843956115
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-12-17 22:49:41 +0700

    fix logoutHandle on client

--- client/customer/handleLogout.js
+++ client/customer/handleLogout.js
@@ -2,7 +2,7 @@ $('#logout').on('click', function() {
     var KonfirmasiUser = confirm('Apakah anda yakin ingin logout?');
     if(KonfirmasiUser){
         localStorage.removeItem('token');
-        window.location.href="auth/login.html";
+        window.location.href="/client/customer/auth/login.html";
     }
     
   });
